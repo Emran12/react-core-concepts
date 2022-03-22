2b5c707c10d49382de5b8402d34c06a8691a218a
Emran12 <emran6452@gmail.com>
2022-03-22 08:10:57 +0600

    Countries have been loaded successfully

--- src/App.js
+++ src/App.js
@@ -2,104 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-const books = [
-    {name: 'one minute madrasha', publication: 'al-arab prokashoni'}, 
-    {name: 'higher math', publication: 'the royal'}
-]
 
-const products = [
-    {name: 'laptop', price: 15300}, 
-    {name: 'laptop', price: 15300}, 
-    {name: 'laptop', price: 15300}, 
-    {name: 'laptop', price: 15300}, 
-]
 
 function App() {
-    const players = ['sakib', 'tamim', 'taskin', 'rubel']; 
     return (
-        <div className='container'>
-            {/* {
-                players.map(player => <Person name={player}></Person>)
-            }
-            {
-                books.map(book => <Library name = {book.name} publication = {book.publication}></Library>)
-            }
-            {
-                products.map(product => <Product name = {product.name} price = {product.price}></Product>)
-            }
-            <Counter></Counter> */}
-            <ExternalUsers></ExternalUsers>
-        </div> 
-        
-    );
-}
-
-function Person (props) {
-    return (
-        <div className='info'>
-            <h1>{props.name}</h1>
-        </div>
-       
-    );
-}
-
-function Library (props) {
-    return (
-        <div className='info'>
-            <h1>{props.name}</h1>
-            <h3>{props.publication}</h3>
+        <div className='App'>
+            <div>
+                <LoadCountries></LoadCountries>
+            </div>
+            
         </div>
     )
 }
 
-function Product (props){
-    return (
-        <div className='info'>
-            <h1>{props.name}</h1>
-            <h3>{props.price}</h3>
-        </div>
-    )
-}
-
-function Counter (){
-    const [count, setCount] = useState(100);
-    const IncreaseValue = () => setCount(count + 1); 
-    const DecreaseValue = () => setCount(count - 1);
-    
-    return (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={IncreaseValue}>Increase</button>
-            <button onClick={DecreaseValue}>Decrease</button>
-        </div>
-    )
-}
-
-function ExternalUsers (){
-    const [users, setUsers] = useState([]); 
+function LoadCountries (){
+    const [countries, setCountries] = useState([]);
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => setUsers(data))
+        .then (data => setCountries(data));
     },[])
     return (
         <div>
-            <h1>External Users</h1>
-            <p>{users.length}</p>
-            {
-                users.map(user => <User name={user.name} email = {user.email}></User>)
+            <h1>Total Countries: {countries.length}</h1>
+            <div className='container'>
+                {
+                countries.map(country => 
+                   <DisplayCountries name={country.name.common}continents = {country.continents} flag = {country.flags.png} population = {country.population}></DisplayCountries>
+                )
             }
+            </div>
+            
         </div>
     )
-
 }
-
-function User (props) {
+function DisplayCountries (props){
     return (
-        <div style={{color:"green", border: "2px solid black", borderRadius: "10px", margin: "20px", textAlign: "center"}}> 
-            <h1> Name:{props.name}</h1>
-            <p> Email: {props.email}</p>
+        <div className='countries'>
+             <h1>Name: {props.name}</h1>
+              <p> <img style={{width:"300px", height:"300px"}} src= {props.flag} alt="" /> </p>
+             <p>Continents: {props.continents}</p>
+             <p>Population: {props.population}</p>  
         </div>
     )
+
 }
 export default App;
